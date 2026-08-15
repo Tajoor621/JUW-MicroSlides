2e47e2a5242ad3842584d1d8ed3a2677555d30f7
NoorGull <ssaim3456@gmail.com>
2026-08-15 12:07:17 +0400

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE = "juw-microslides-v2-1";
+const ASSETS = ["./", "./index.html", "./manifest.json", "./sw.js", "./assets/logo-juw.jpg", "./assets/icon-192x192.png", "./assets/icon-512x512.png"];
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener("activate", (e) => {
+  e.waitUntil(caches.keys().then((keys) => Promise.all(keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener("fetch", (e) => {
+  if (e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then((cached) => {
+      const net = fetch(e.request).then((res) => {
+        if (res && res.ok && res.type === "basic") {
+          const copy = res.clone();
+          caches.open(CACHE).then((c) => c.put(e.request, copy));
+        }
+        return res;
+      }).catch(() => cached || caches.match("./index.html"));
+      return cached || net;
+    })
+  );
+});
